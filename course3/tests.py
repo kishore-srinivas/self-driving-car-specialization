2240e20c810da7178083032e938f54915d3e1fca
kishore-srinivas <kishores4200@gmail.com>
2020-12-18 13:20:36 -0800

    finished final project

--- course3/tests.py
+++ course3/tests.py
@@ -1,32 +1,63 @@
-import cv2
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-og = cv2.imread('C:\\Users\\kisho\\Documents\\git\\projects\\image-processing\\images\\line.jpg')
-im = cv2.cvtColor(og, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('im', im)
+N_POINTS = 10
+TARGET_X_SLOPE = 2
+TARGET_y_SLOPE = 3
+TARGET_OFFSET  = 5
+EXTENTS = 5
+NOISE = 5
 
-dst = cv2.cornerHarris(im, 2, 3, 0.04)
-# cv2.imshow('kp', dst)
+# [[ 0.81  0.9   0.73  0.52  0.96]
+#  [ 0.93 -0.84  0.85 -1.34  2.26]
+#  [ 2.47  1.96  2.79  3.47  1.94]]
 
-dst_norm = np.empty(dst.shape, dtype=np.float32)
-cv2.normalize(dst, dst_norm, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-dst_norm_scaled = cv2.convertScaleAbs(dst_norm)
-print(dst_norm_scaled.shape)
-# print(np.max(dst_norm_scaled), np.where(dst_norm_scaled == np.max(dst_norm_scaled)))
-# print(np.min(dst_norm_scaled), np.where(dst_norm_scaled == np.min(dst_norm_scaled)))
+# create random data
+xs = [0.81, 0.9, 0.73, 0.52, 0.96]
+ys = [0.93, -0.84,  0.85, -1.34,  2.26]
+zs = [2.47,  1.96,  2.79,  3.47,  1.94]
+# for i in range(N_POINTS):
+#     zs.append(xs[i]*TARGET_X_SLOPE + \
+#               ys[i]*TARGET_y_SLOPE + \
+#               TARGET_OFFSET + np.random.normal(scale=NOISE))
 
-threshold = 195
-feats = np.zeros_like(im)
-kp = []
-points = np.where(dst_norm_scaled > threshold)
-for i in range(len(points[0])):
-    kp.append(cv2.KeyPoint(points[1][i], points[0][i], 5))
-cv2.imshow('feats', cv2.drawKeypoints(im, kp, None))
+# plot raw data
+plt.figure()
+ax = plt.subplot(111, projection='3d')
+ax.scatter(xs, ys, zs, color='b')
 
-arr = np.linspace(0, 10, 12).reshape(4, 3)
-points = np.where(arr > 5)
-for i in range(len(points[0])):
-    arr[points[0][i], points[1][i]] = 100
-print(arr)
+# do fit
+tmp_A = []
+tmp_b = []
+for i in range(len(xs)):
+    tmp_A.append([xs[i], ys[i], zs[i]])
+    tmp_b.append(1)
+b = np.matrix(tmp_b).T
+A = np.matrix(tmp_A)
+fit = (A.T * A).I * A.T * b
+errors = b - A * fit
+residual = np.linalg.norm(errors)
 
-cv2.waitKey()
+print("solution:")
+print("%f x + %f y + %f z = 1" % (fit[0], fit[1], fit[2]))
+print("errors:")
+print(errors)
+print("residual:")
+print(residual)
+
+# plot plane
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+X,Y = np.meshgrid(np.arange(xlim[0], xlim[1]),
+                  np.arange(ylim[0], ylim[1]))
+Z = np.zeros(X.shape)
+# for r in range(X.shape[0]):
+#     for c in range(X.shape[1]):
+#         Z[r,c] = (1 - fit[0] * X[r,c] + fit[1] * Y[r,c]) / fit[2]
+ax.plot_wireframe(X,Y,Z, color='k')
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
